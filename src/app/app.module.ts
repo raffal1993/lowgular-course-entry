a82d2cfbac10f29c5661a85cc989040ea4171c03
raffal1993 <raffal1993@gmail.com>
2022-11-16 20:47:07 +0100

    remove files, prepare for new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,8 @@
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { EmployeeListComponentModule } from "./ui/employee-list/employee-list.component-module";
-import {EmployeesFacesComponentModule} from "./ui/employees-faces/employees-faces.component-module";
-import {EmployeeServiceModule} from "./services/employee.service-module";
-import {ProjectListComponentModule} from "./ui/project-list/project-list.component-module";
-import {ProjectsServiceModule} from "./services/projects.service-module";
-import {EmployeeFormComponentModule} from "./ui/employee-form/employee-form.component-module";
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,14 +10,10 @@ import {EmployeeFormComponentModule} from "./ui/employee-form/employee-form.comp
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    EmployeeListComponentModule,
-    EmployeesFacesComponentModule,
-    EmployeeServiceModule,
-    ProjectListComponentModule,
-    ProjectsServiceModule,
-    EmployeeFormComponentModule
+
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/app/model/employeeResponse.model.ts
+++
@@ -1,7 +0,0 @@
-export interface EmployeeResponse {
-  id: string,
-  employee_name: string,
-  employee_salary: string,
-  employee_age: string,
-  profile_image: string;
-}
